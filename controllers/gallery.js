292452915cf77ad9fd20c0462a1c595fa832b4b5
Praveen <praveenkumarpatini@gmail.com>
2023-02-25 23:14:26 +0530

    modified gallery

--- controllers/gallery.js
+++ controllers/gallery.js
@@ -1,10 +1,10 @@
 const cloudinary = require("cloudinary").v2;
 
-exports.getImages = async (req, res) => {
+/*exports.getImages = async (req, res) => {
   try {
     const images = await cloudinary.search
       .expression("folder:Home")
-      .max_results(110)
+      .max_results(210)
       .execute();
 
     const data = images.resources;
@@ -23,18 +23,22 @@ exports.getImages = async (req, res) => {
       message: err.message,
     });
   }
-};
+};*/
 
 exports.cloudurl = async (req, res) => {
 
 const searchOptions = {expression: 'folder:Home', sort_by: 'public_id', order: 'desc'};
 cloudinary.search
   .expression(searchOptions.expression)
-  .max_results(110)
+  .max_results(300)
   .execute()
   .then(result => {
     const imageUrls = result.resources.map(resource => resource.url);
-   
+    for (let i = imageUrls.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [imageUrls[i], imageUrls[j]] = [imageUrls[j], imageUrls[i]];
+    }
+   console.log(imageUrls)
     res.json(imageUrls);
   })
   .catch(error => {
